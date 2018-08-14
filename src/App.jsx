d2a19aebc947fa760651242f3e3a776bdeb564eb
Andrew Cline <andrew.cline77@gmail.com>
2018-08-14 14:05:58 -0500

    Update for App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
 
+import './css/App.css'
+
 export default class App extends Component {
     render() {
-        return <div className="App"><h1>Hello World!</h1></div>
+        return (
+            <div className="App">
+                <h1>Hello World!</h1>
+            </div>
+        )
     }
 }
