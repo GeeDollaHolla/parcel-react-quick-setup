6fe781f642fda06d12f16160e17451ea4241a734
Andrew Cline <andrew.cline77@gmail.com>
2018-08-14 12:31:59 -0500

    Setup for tests, change to App

--- src/js/App.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react'
-
-export default class App extends Component {
-    render() {
-        const string = ['This is a JavaScript String', 'This is a new line.']
-        return <div className="App">{string.map(i => <h1>{i}</h1>)}</div>
-    }
-}
